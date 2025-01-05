012a906f5dbb628b8b0b2c2bb8426f103f4dc455
sauki alvaro <saukialvaro@gmail.com>
2025-01-06 04:15:24 +0700

    update

--- resources/views/frontend/portfolio-detail/asset_konsep_gaya-detail.blade.php
+++ resources/views/frontend/portfolio-detail/asset_konsep_gaya-detail.blade.php
@@ -19,7 +19,7 @@
 
   <!-- Vendor CSS Files -->
   <link href="frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  {{--  <link href="frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">  --}}
+  <link href="frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="frontend/vendor/aos/aos.css" rel="stylesheet">
   <link href="frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
--- resources/views/frontend/portfolio.blade.php
+++ resources/views/frontend/portfolio.blade.php
@@ -19,7 +19,7 @@
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset ('frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  {{--  <link href="{{ asset ('frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">  --}}
+  <link href="{{ asset ('frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
   <link href="{{ asset ('frontend/vendor/aos/aos.css') }}" rel="stylesheet">
   <link href="{{ asset ('frontend/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
   <link href="{{ asset ('frontend/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
