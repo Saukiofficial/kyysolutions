90bed9e262d74417008a6e878ddd7eba4e5a84b6
sauki alvaro <saukialvaro@gmail.com>
2025-01-06 02:59:17 +0700

    update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -115,7 +115,7 @@
       </div>
     </div>
   </div>
-  <img src="{{ asset ('frontend/img/sementara.jpg') }}" alt="" data-aos="fade-in" style="width: 100%; height: 100vh; object-fit: cover; position: absolute; top: 0; left: 0;">
+  <video autoplay loop muted src="{{ asset ('frontend/img/kyylogo.mp4') }}" alt="" data-aos="fade-in" style="width: 100%; height: 100vh; object-fit: cover; position: absolute; top: 0; left: 0;">
 </section>
 
 <script>
